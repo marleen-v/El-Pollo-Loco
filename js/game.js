14f0bc6718f967b76c411535b31a32098dc803e7
marleen-v <marleen.vaalbrock@gmail.com>
2024-11-20 14:31:26 +0100

    key shortcuts

--- js/game.js
+++ js/game.js
@@ -1,3 +1,5 @@
+const imgRef = document.querySelectorAll(".key");
+
 let canvas;
 let world;
 let keyboard = new Keyboard();
@@ -11,12 +13,16 @@ function init() {
 window.addEventListener('keydown', (event) => {
     if(event.key == 'ArrowRight'){
         keyboard.RIGHT = true;
+        toggleDisplay(5, 6);
+
     };
     if(event.key == 'ArrowLeft'){
         keyboard.LEFT = true;
+        toggleDisplay(2, 3);
     };
     if(event.key == 'ArrowUp'){
         keyboard.UP = true;
+        toggleDisplay(0, 1);
     };
     if(event.key == 'ArrowDown'){
         keyboard.DOWN = true;
@@ -26,18 +32,25 @@ window.addEventListener('keydown', (event) => {
     };
     if(event.key == 'd'){
         keyboard.D = true;
+          toggleDisplay(7, 8);
     };
 } ) 
 
 window.addEventListener('keyup', (event) => {
     if(event.key == 'ArrowRight'){
         keyboard.RIGHT = false;
+        toggleDisplay(5, 6);
+       
+
     };
     if(event.key == 'ArrowLeft'){
         keyboard.LEFT = false;
+        toggleDisplay(2, 3);
+        
     };
     if(event.key == 'ArrowUp'){
         keyboard.UP = false;
+        toggleDisplay(0, 1);
     };
     if(event.key == 'ArrowDown'){
         keyboard.DOWN = false;
@@ -47,6 +60,12 @@ window.addEventListener('keyup', (event) => {
     };
     if(event.key == 'd'){
         keyboard.D = false;
+        toggleDisplay(7, 8);
     };
 } ) 
 
+function toggleDisplay(img, img_active) {
+    imgRef[img].classList.toggle("d_none");
+    imgRef[img_active].classList.toggle("d_none");
+}
+  
